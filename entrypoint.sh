d4287b2481b7618d4bd131e797c283fa5aa306d5
Jeroen Geusebroek <me@jeroengeusebroek.nl>
2018-02-21 08:37:14 +0100

    Fix config overwrite

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,9 @@
 addgroup -g ${GID} privatebin && adduser -h /privatebin -s /bin/sh -D -G privatebin -u ${UID} privatebin
 touch /var/run/php-fpm.sock
 
-if [ ! -f /privatebin/cfg/conf.ini ]; then
+if [ ! -f /privatebin/cfg/conf.php ]; then
 	cp /privatebin/conf.sample.php /privatebin/cfg/conf.php
 fi
 
 chown -R privatebin:privatebin /privatebin /var/run/php-fpm.sock /var/lib/nginx /tmp /var/tmp/nginx
 supervisord -c /usr/local/etc/supervisord.conf
-
